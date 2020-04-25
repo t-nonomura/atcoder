7144db72c77abc87c4adb8e9404fe155f70f7d2f
t-nonomura <nono.develop@gmail.com>
2020-04-25 23:51:29 +0900

    initial commit

---
+++ src/com/treeengineering/atcoder/Tutorial.kt
@@ -0,0 +1,8 @@
+package com.treeengineering.atcoder
+
+fun main(args: Array<String>) {
+    val a = readLine()!!.toInt()
+    val (b,c) = readLine()!!.split(" ").map { it.toInt() }
+    val s = readLine() ?: ""
+    println("${a + b + c} $s")
+}
