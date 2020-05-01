321d3d7d9f6b71315687c83da6d644343ff5ec1e
t-nonomura <nono.develop@gmail.com>
2020-05-01 12:38:05 +0900

    TLE改善

--- src/main/java/com/treeengineering/atcoder/abc/081B.kt
+++ src/main/java/com/treeengineering/atcoder/abc/081B.kt
@@ -1,14 +1,25 @@
 package main.java.com.treeengineering.atcoder.abc
 
-// TODO TLE改善
 fun main(args: Array<String>) {
     readLine()
-    val numbers = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    var numbers = readLine()!!.split(" ").map { it.toInt() }
 
-    var count = 0
-    while (numbers.filter { it > 0 && it % 2 == 0 }.size == numbers.size) {
-        count++
-        numbers.map { it / 2 }
+    var c = 0
+
+    var flag = true
+    while (true) {
+        for (n in numbers) {
+            if (n % 2 == 1) {
+                flag = false
+                break
+            }
+        }
+        if (flag) {
+            numbers = numbers.map { it / 2 }
+            c++
+        } else {
+            break
+        }
     }
-    println("$count")
+    println("$c")
 }
